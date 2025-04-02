36b929122fb69ed08fa3cf3aa4a161a5c5499def
abdul falaq <falaq@logique.co.id>
2025-04-02 19:32:29 +0700

    Fix: menambahkan cors untuk di akses dari FE

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./docs/swaggerConfig');
 
 const app = express();
+
+// Implementasi CORS untuk semua origin
+app.use(cors());
+
+// Middleware
 app.use(express.json());
 
 // Dokumentasi Swagger
@@ -13,5 +19,13 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Rute Produk
 app.use('/products', productRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Terjadi kesalahan pada server' });
+});
+
+const PORT = process.env.PORT || 9494;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
+});
